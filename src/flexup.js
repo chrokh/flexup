e4147b94c51df78aea06fb88e4277fe2ecc13f27
Christopher Okhravi <christopher.okhravi@gmail.com>
2014-08-09 09:52:18 +0200

    Use JSON instead of eval

--- src/flexup.js
+++ src/flexup.js
@@ -2,31 +2,21 @@ module.exports = (function(){
   var Flexup,
       Pattern = require('./pattern.js'),
       Library = require('./library.js'),
-      doc,
-      instance;
+      doc;
 
   Flexup = function(doc, definitions){
     this.doc         = doc;
-    this.definitions = definitions;
+    this.definitions = JSON.parse(definitions);
     this.library = new Library();
   }
 
   Flexup.prototype.read = function(){
-    Flexup.instance = this;
-    eval(this.definitions);
+    for(var key in this.definitions){
+      var pattern = new Pattern(key, this.definitions[key]);
+      this.library.add(pattern);
+    }
     return this.library.execute(this.doc);
   }
-
-
-
-  /*
-   * DSL function
-   */
-  function define(name, pattern){
-    var pattern = new Pattern(name, pattern);
-    Flexup.instance.library.add(pattern);
-  }
-  
 
   return Flexup;
 })();
